acc4cead34b866186c6e6d176ef5bed5192977c5
liaemilia16 <liaemilia269@gmail.com>
2024-01-06 21:25:32 +0700

    Upload Project kampus

---
+++ resources/views/front.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0"> Welcome</h1>
+                </div>
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Kampus App</h5>
+
+                            <p class="card-text">
+                                Kampus App adalah sebuah aplikasi yang dirancang khusus untuk membantu mahasiswa dan anggota komunitas kampus dalam mengakses beragam informasi yang berkaitan dengan kehidupan akademik dan kegiatan di lingkungan universitas atau perguruan tinggi. Aplikasi ini menawarkan berbagai fitur yang mencakup jadwal kuliah, peta kampus, informasi akademik, pengumuman, serta akses ke sumber daya perpustakaan digital. Melalui Kampus App, pengguna dapat dengan mudah mengetahui jadwal kuliah, lokasi ruang kuliah, dan mengakses materi akademik secara praktis. Fitur komunikasi dan kolaborasi yang disediakan juga memungkinkan mahasiswa, dosen, dan staf kampus untuk berinteraksi, berbagi informasi, serta berdiskusi mengenai topik akademik dan kegiatan kampus secara langsung melalui platform aplikasi ini.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+@endsection
